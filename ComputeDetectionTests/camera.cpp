bc0c5191b8f766690fe1cf7e6665af1ea6346c12
Rune Ø. Schrøder <114364331+rune1750@users.noreply.github.com>
2024-01-07 13:18:12 +0100

    Update camera.cpp

--- ComputeDetectionTests/camera.cpp
+++ ComputeDetectionTests/camera.cpp
@@ -1,5 +1,7 @@
 #include "camera.h"
 #include <cstdlib>
+
+
 camera::camera()
 {
 }
@@ -8,8 +10,16 @@ camera::~camera()
 {
 }
 
-// return a 9*9 matrix of random integers between 0 and 9
-long camera::cam_data()
+int camera::cam_data()
 {
-    return xorshf96() % 10;
+    int matrix[9][9];
+    cam_data_label1:for(int i=0; i<9; i++)
+    {
+        cam_data_label0:for(int j=0; j<9; j++)
+        {
+            matrix[i][j] = xorshf96() % 10;
+        }
+    }
+
+    return matrix[8][8];
 }
