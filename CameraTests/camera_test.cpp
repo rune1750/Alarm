415a8f5b291b38221aa5e75477fc58fefcf88267
Rune Ø. Schrøder <114364331+rune1750@users.noreply.github.com>
2024-01-07 13:17:20 +0100

    Update camera_test.cpp

--- CameraTests/camera_test.cpp
+++ CameraTests/camera_test.cpp
@@ -1,27 +1,15 @@
 #include "camera.h"
-#include <chrono>
 #include <iostream>
 
 int main(){
     camera C;
-    int dim = 9;
-    auto start = std::chrono::high_resolution_clock::now();
-    int* matrix = C.cam_data(dim);
-    // end timer
-    auto end = std::chrono::high_resolution_clock::now();
-    // print time elapsed
-    int matrix1[9][9];
-    for(int i=0; i<dim; i++)
+    int matrix[9][9];
+    for(int i=0; i<9; i++)
     {
-        for(int j=0; j<dim; j++)
+        for(int j=0; j<9; j++)
         {
-            matrix1[i][j] = *matrix;
-            matrix++;
-
+            matrix[i][j] = C.cam_data();
         }
-
-    }    
-    std::cout << "Time elapsed: " << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() << "ns\n";
+    }
     return 0;
 }
-
