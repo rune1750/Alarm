2452b535f8f3c8915c2ce226715a2f44d1c736fc
Rune Ø. Schrøder <114364331+rune1750@users.noreply.github.com>
2024-01-07 13:16:10 +0100

    Update camera.cpp

--- CameraTests/camera.cpp
+++ CameraTests/camera.cpp
@@ -11,19 +11,18 @@ camera::~camera()
 }
 
 // return a 9*9 matrix of random integers between 0 and 9
-int* camera::cam_data(int dim)
-{      
-    int matrix[9*9];
-    for(int i=0; i<dim; i++)
+int camera::cam_data()
+{
+    int matrix[9][9];
+    cam_data_label1:for(int i=0; i<9; i++)
     {
-        for(int j=0; j<dim; j++)
+        cam_data_label0:for(int j=0; j<9; j++)
         {
-            matrix[ dim * i + j ] = xorshf96() % 10;
-
+            matrix[i][j] = xorshf96() % 10;
+            //std::cout << matrix[i][j]<< " ";
         }
-
+        //std::cout<< '\n';
     }
-    int* point = &matrix[0];
     
-    return point;
+    return matrix[8][8];
 }
